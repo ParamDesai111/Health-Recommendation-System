eb60b063d859dddd2af1d90486717b69d1eb7980
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-07-27 16:07:03 -0400

    changes to host on vercel

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template, jsonify
 import numpy as np
 import pickle
 import pandas as pd
+import os
 
 # Run Flask App
 app = Flask(__name__)
@@ -97,4 +98,6 @@ def about():
 
 # Python main function
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
